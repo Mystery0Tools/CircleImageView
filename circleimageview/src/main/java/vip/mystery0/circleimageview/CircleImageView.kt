b961c30816f3a9a54d218e102c53f2e2454944ec
Mystery0 <mystery0dyl520@gmail.com>
2019-02-26 20:15:30 +0800

    添加额外的角标

--- circleimageview/src/main/java/vip/mystery0/circleimageview/CircleImageView.kt
+++ circleimageview/src/main/java/vip/mystery0/circleimageview/CircleImageView.kt
@@ -7,7 +7,11 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import kotlin.math.max
 import kotlin.math.min
+import android.graphics.RectF
+
 
 class CircleImageView(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
 		ImageView(context, attrs, defStyleAttr) {
@@ -17,50 +21,135 @@ class CircleImageView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 	private var bitmapHeight = 0
 	private lateinit var bitmapPaint: Paint//绘制bitmap的画笔
 	private lateinit var borderPaint: Paint//绘制边框的画笔
+	private lateinit var additionalPaint: Paint//绘制额外图片的画笔
 	private var resourceBitmap: Bitmap? = null//存储bitmap
+	private var additionalBitmap: Bitmap? = null//存储额外的图片
+	private lateinit var additionalRectF: RectF
 
 	constructor(context: Context) : this(context, null)
 	constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
 	init {
 		val config = getConfig()
-		val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView, defStyleAttr, 0)
-		if (typedArray.hasValue(R.styleable.CircleImageView_draw_circle))
-			config.drawCircle = typedArray.getBoolean(R.styleable.CircleImageView_draw_circle, true)
-		if (typedArray.hasValue(R.styleable.CircleImageView_draw_border))
-			config.drawBorder = typedArray.getBoolean(R.styleable.CircleImageView_draw_border, true)
-		if (typedArray.hasValue(R.styleable.CircleImageView_border_width))
-			config.borderWidth = typedArray.getDimension(R.styleable.CircleImageView_border_width, config.borderWidth)
-		if (typedArray.hasValue(R.styleable.CircleImageView_border_color))
-			config.borderColor = typedArray.getColor(R.styleable.CircleImageView_border_color, config.borderColor)
-		if (typedArray.hasValue(R.styleable.CircleImageView_circle_radius))
-			config.circleRadius = typedArray.getDimension(R.styleable.CircleImageView_circle_radius, config.circleRadius)
-		if (typedArray.hasValue(R.styleable.CircleImageView_center_x))
-			config.centerX = typedArray.getFloat(R.styleable.CircleImageView_center_x, config.centerX)
-		if (typedArray.hasValue(R.styleable.CircleImageView_center_y))
-			config.centerY = typedArray.getFloat(R.styleable.CircleImageView_center_y, config.centerY)
-		typedArray.recycle()
+		val a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView, defStyleAttr, 0)
+		if (a.hasValue(R.styleable.CircleImageView_draw_circle))
+			config.isDrawCircle(a.getBoolean(R.styleable.CircleImageView_draw_circle, config.drawCircle))
+		if (a.hasValue(R.styleable.CircleImageView_draw_border))
+			config.isDrawBorder(a.getBoolean(R.styleable.CircleImageView_draw_border, config.drawBorder))
+		if (a.hasValue(R.styleable.CircleImageView_border_width))
+			config.setBorderWidth(a.getDimension(R.styleable.CircleImageView_border_width, config.borderWidth))
+		if (a.hasValue(R.styleable.CircleImageView_border_color))
+			config.setBorderColor(a.getColor(R.styleable.CircleImageView_border_color, config.borderColor))
+		if (a.hasValue(R.styleable.CircleImageView_circle_radius))
+			config.setCircleRadius(a.getDimension(R.styleable.CircleImageView_circle_radius, config.circleRadius))
+		if (a.hasValue(R.styleable.CircleImageView_center_x))
+			config.setCenter(centerX = a.getFloat(R.styleable.CircleImageView_center_x, config.centerX))
+		if (a.hasValue(R.styleable.CircleImageView_center_y))
+			config.setCenter(centerY = a.getFloat(R.styleable.CircleImageView_center_y, config.centerY))
+		if (a.hasValue(R.styleable.CircleImageView_additional_src))
+			setAdditionalImageDrawable(a.getDrawable(R.styleable.CircleImageView_additional_src))
+		if (a.hasValue(R.styleable.CircleImageView_additional_location))
+			config.setAdditionalLocation(a.getInteger(R.styleable.CircleImageView_additional_location, config.additionalLocation))
+		if (a.hasValue(R.styleable.CircleImageView_additional_width))
+			config.setAdditionalSize(width = a.getDimension(R.styleable.CircleImageView_additional_width, config.additionalWidth))
+		if (a.hasValue(R.styleable.CircleImageView_additional_height))
+			config.setAdditionalSize(height = a.getDimension(R.styleable.CircleImageView_additional_height, config.additionalHeight))
+		if (a.hasValue(R.styleable.CircleImageView_additional_margin_horizontal))
+			config.setAdditionalMargin(marginHorizontal = a.getDimension(R.styleable.CircleImageView_additional_margin_horizontal, config.additionalMarginHorizontal))
+		if (a.hasValue(R.styleable.CircleImageView_additional_margin_vertical))
+			config.setAdditionalMargin(marginVertical = a.getDimension(R.styleable.CircleImageView_additional_margin_vertical, config.additionalMarginVertical))
+		a.recycle()
+		updateConfig()
 	}
 
 	@Synchronized
-	fun getConfig(): CircleImageViewConfig {
+	private fun getConfig(): CircleImageViewConfig {
 		if (!::viewConfig.isInitialized)
 			viewConfig = CircleImageViewConfig()
 		return viewConfig
 	}
 
 	fun setConfig(config: CircleImageViewConfig): CircleImageView {
-		val viewConfig=getConfig()
-		this.viewConfig.copy(config)
+		getConfig().copy(config)
+		updateConfig()
 		return this
 	}
 
 	fun config(listener: (CircleImageViewConfig) -> CircleImageViewConfig): CircleImageView =
 			setConfig(listener.invoke(getConfig()))
 
+	private fun updateConfig() {
+		val config = getConfig()
+		updatePaintConfig(config)
+		updateAdditionalConfig(config)
+		invalidate()
+	}
+
+	private fun updatePaintConfig(config: CircleImageViewConfig) {
+		if (config.circleRadius == -1F) {
+			config.setCircleRadius(min(bitmapHeight, bitmapWidth) / 2F)
+		}
+		borderPaint.color = config.borderColor
+		borderPaint.strokeWidth = config.borderWidth
+	}
+
+	private fun updateAdditionalConfig(config: CircleImageViewConfig) {
+		if (additionalBitmap == null)
+			return
+		config.setAdditionalSize(width = max(additionalBitmap!!.width.toFloat(), config.additionalWidth), height = max(additionalBitmap!!.height.toFloat(), config.additionalHeight))
+		val marginRectF = RectF(
+				config.additionalMarginHorizontal,
+				config.additionalMarginVertical,
+				bitmapWidth - config.additionalMarginHorizontal,
+				bitmapHeight - config.additionalMarginVertical)
+		additionalRectF = when (config.additionalLocation) {
+			CircleImageViewConfig.Location.LEFT -> RectF(
+					marginRectF.left,
+					(marginRectF.top + marginRectF.bottom - config.additionalHeight) / 2,
+					marginRectF.left + config.additionalWidth,
+					(marginRectF.top + marginRectF.bottom + config.additionalHeight) / 2)
+			CircleImageViewConfig.Location.TOP -> RectF(
+					(marginRectF.left + marginRectF.right - config.additionalWidth) / 2,
+					marginRectF.top,
+					(marginRectF.left + marginRectF.right + config.additionalWidth) / 2,
+					marginRectF.top + config.additionalHeight)
+			CircleImageViewConfig.Location.RIGHT -> RectF(
+					marginRectF.right - config.additionalWidth,
+					(marginRectF.top + marginRectF.bottom - config.additionalHeight) / 2,
+					marginRectF.right,
+					(marginRectF.top + marginRectF.bottom + config.additionalHeight) / 2)
+			CircleImageViewConfig.Location.BOTTOM -> RectF(
+					(marginRectF.left + marginRectF.right - config.additionalWidth) / 2,
+					marginRectF.bottom - config.additionalHeight,
+					(marginRectF.left + marginRectF.right + config.additionalWidth) / 2,
+					marginRectF.bottom)
+			CircleImageViewConfig.Location.LEFT_TOP -> RectF(
+					marginRectF.left,
+					marginRectF.top,
+					marginRectF.left + config.additionalWidth,
+					marginRectF.top + config.additionalHeight)
+			CircleImageViewConfig.Location.RIGHT_TOP -> RectF(
+					marginRectF.right - config.additionalWidth,
+					marginRectF.top,
+					marginRectF.right,
+					marginRectF.top + config.additionalHeight)
+			CircleImageViewConfig.Location.LEFT_BOTTOM -> RectF(
+					marginRectF.left,
+					marginRectF.bottom - config.additionalHeight,
+					marginRectF.left + config.additionalWidth,
+					marginRectF.bottom)
+			CircleImageViewConfig.Location.RIGHT_BOTTOM -> RectF(
+					marginRectF.right - config.additionalWidth,
+					marginRectF.bottom - config.additionalHeight,
+					marginRectF.right,
+					marginRectF.bottom)
+			else -> throw Exception("The value of additional location is error")
+		}
+	}
+
 	override fun onDraw(canvas: Canvas) {
-		val config=getConfig()
-		if (!config.drawCircle)
+		val config = getConfig()
+		if (!config.drawCircle && !config.drawAdditional)
 			return super.onDraw(canvas)
 		canvas.drawCircle(bitmapWidth * config.centerX, bitmapHeight * config.centerY, config.circleRadius, bitmapPaint)
 		if (config.drawBorder) {//绘制边框
@@ -71,6 +160,24 @@ class CircleImageView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 					borderPaint
 			)
 		}
+		if (config.drawAdditional && additionalBitmap != null)
+			canvas.drawBitmap(additionalBitmap!!, null, additionalRectF, additionalPaint)
+	}
+
+	fun setAdditionImageResource(resId: Int) = setAdditionalImageDrawable(ContextCompat.getDrawable(context, resId))
+
+	fun setAdditionalImageDrawable(drawable: Drawable?) {
+		if (drawable == null)
+			return
+		setAdditionalImageBitmap(drawable.toBitmap())
+	}
+
+	fun setAdditionalImageBitmap(bitmap: Bitmap) {
+		additionalBitmap = bitmap
+		additionalPaint = Paint()
+		additionalPaint.isAntiAlias = true
+		additionalPaint.style = Paint.Style.FILL
+		updateConfig()
 	}
 
 	/**
@@ -93,8 +200,8 @@ class CircleImageView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 		}
 		if (this !is BitmapDrawable) {//如果不是BitmapDrawable，需要将drawable画到bitmap上面
 			val canvas = Canvas(bitmap)
-			drawable.setBounds(0, 0, canvas.width, canvas.height)
-			drawable.draw(canvas)
+			this.setBounds(0, 0, canvas.width, canvas.height)
+			this.draw(canvas)
 		}
 		return bitmap
 	}
@@ -106,10 +213,7 @@ class CircleImageView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 		resourceBitmap = drawable.toBitmap()
 		bitmapHeight = resourceBitmap!!.height
 		bitmapWidth = resourceBitmap!!.width
-		val config = getConfig()
-		if (config.circleRadius == -1F) {
-			config.circleRadius = min(bitmapHeight, bitmapWidth) / 2F
-		}
+
 		val bitmapShader = BitmapShader(resourceBitmap!!, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
 		bitmapPaint = Paint()
 		bitmapPaint.isAntiAlias = true
@@ -118,8 +222,6 @@ class CircleImageView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 		borderPaint = Paint()
 		borderPaint.isAntiAlias = true
 		borderPaint.style = Paint.Style.STROKE
-		borderPaint.color = config.borderColor
-		borderPaint.strokeWidth = config.borderWidth
 	}
 
 	companion object {
--- circleimageview/src/main/java/vip/mystery0/circleimageview/CircleImageViewConfig.kt
+++ circleimageview/src/main/java/vip/mystery0/circleimageview/CircleImageViewConfig.kt
@@ -4,66 +4,126 @@ import android.graphics.Color
 import androidx.annotation.ColorInt
 
 class CircleImageViewConfig {
-    var drawCircle = true//是否裁剪成圆形
-    var drawBorder = true//是否绘制边框
-    var borderWidth = 1F//边框宽度
-    var borderColor = Color.BLACK//边框颜色
-    var circleRadius = -1F//圆形的半径
-    var centerX = 0.5F//绘制圆形的圆心X比例，默认是0.5
-    var centerY = 0.5F//绘制圆形的圆心Y比例，默认是0.5
+	object Location {
+		const val LEFT = 1
+		const val TOP = 2
+		const val RIGHT = 3
+		const val BOTTOM = 4
+		const val LEFT_TOP = 5
+		const val RIGHT_TOP = 6
+		const val LEFT_BOTTOM = 7
+		const val RIGHT_BOTTOM = 8
+	}
 
-    var isShowAdditional=false//是否显示额外信息（Pro装饰）
+	var drawCircle = true
+		//是否裁剪成圆形
+		private set
+	var drawBorder = true
+		//是否绘制边框
+		private set
+	var drawAdditional = true
+		//是否显示额外信息（Pro装饰）
+		private set
+	var borderWidth = 1F
+		//边框宽度
+		private set
+	var borderColor = Color.BLACK
+		//边框颜色
+		private set
+	var circleRadius = -1F
+		//圆形的半径
+		private set
+	var centerX = 0.5F
+		//绘制圆形的圆心X比例，默认是0.5
+		private set
+	var centerY = 0.5F
+		//绘制圆形的圆心Y比例，默认是0.5
+		private set
 
-    fun isDrawCircle(isDrawCircle: Boolean): CircleImageViewConfig {
-        drawCircle = isDrawCircle
-        return this
-    }
+	var additionalLocation = Location.RIGHT_TOP
+		private set
+	var additionalWidth = 80F
+		private set
+	var additionalHeight = 40F
+		private set
+	var additionalMarginHorizontal = 20F
+		private set
+	var additionalMarginVertical = 20F
+		private set
 
-    fun isDrawBorder(isDrawBorder: Boolean): CircleImageViewConfig {
-        drawBorder = isDrawBorder
-        return this
-    }
+	fun isDrawCircle(isDrawCircle: Boolean): CircleImageViewConfig {
+		drawCircle = isDrawCircle
+		return this
+	}
 
-    fun setBorderWidth(borderWidth: Float): CircleImageViewConfig {
-        if (borderWidth < 0F)
-            throw RuntimeException("border width cannot be less than zero.")
-        this.borderWidth = borderWidth
-        return this
-    }
+	fun isDrawBorder(isDrawBorder: Boolean): CircleImageViewConfig {
+		drawBorder = isDrawBorder
+		return this
+	}
 
-    fun setBorderColor(@ColorInt borderColor: Int): CircleImageViewConfig {
-        this.borderColor = borderColor
-        return this
-    }
+	fun isDrawAdditional(isDrawAdditional: Boolean): CircleImageViewConfig {
+		this.drawAdditional = isDrawAdditional
+		return this
+	}
 
-    fun setCircleRadius(circleRadius: Float): CircleImageViewConfig {
-        if (circleRadius < 0F)
-            throw RuntimeException("circle radius cannot be less than zero.")
-        this.circleRadius = circleRadius
-        return this
-    }
+	fun setBorderWidth(borderWidth: Float): CircleImageViewConfig {
+		if (borderWidth < 0F)
+			throw RuntimeException("border width cannot be less than zero.")
+		this.borderWidth = borderWidth
+		return this
+	}
 
-    fun setCenterX(centerX: Float): CircleImageViewConfig {
-        this.centerX = centerX
-        if (this.centerX < 0F) this.centerX = 0F
-        if (this.centerX > 1F) this.centerX = 1F
-        return this
-    }
+	fun setBorderColor(@ColorInt borderColor: Int): CircleImageViewConfig {
+		this.borderColor = borderColor
+		return this
+	}
 
-    fun setCenterY(centerY: Float): CircleImageViewConfig {
-        this.centerY = centerY
-        if (this.centerY < 0F) this.centerY = 0F
-        if (this.centerY > 1F) this.centerY = 1F
-        return this
-    }
+	fun setCircleRadius(circleRadius: Float): CircleImageViewConfig {
+		if (circleRadius < 0F)
+			throw RuntimeException("circle radius cannot be less than zero.")
+		this.circleRadius = circleRadius
+		return this
+	}
 
-    fun copy(config: CircleImageViewConfig) {
-        isDrawCircle(config.drawCircle)
-        isDrawBorder(config.drawBorder)
-        setBorderWidth(config.borderWidth)
-        setBorderColor(config.borderColor)
-        setCircleRadius(config.circleRadius)
-        setCenterX(config.centerX)
-        setCenterY(config.centerY)
-    }
+	fun setCenter(centerX: Float = this.centerX, centerY: Float = this.centerY): CircleImageViewConfig {
+		this.centerX = centerX
+		if (this.centerX < 0F) this.centerX = 0F
+		if (this.centerX > 1F) this.centerX = 1F
+		this.centerY = centerY
+		if (this.centerY < 0F) this.centerY = 0F
+		if (this.centerY > 1F) this.centerY = 1F
+		return this
+	}
+
+	fun setAdditionalLocation(location: Int): CircleImageViewConfig {
+		if (location !in Location.LEFT..Location.RIGHT_BOTTOM)
+			throw Exception("input is not a valid value")
+		additionalLocation = location
+		return this
+	}
+
+	fun setAdditionalSize(width: Float = this.additionalWidth, height: Float = this.additionalHeight): CircleImageViewConfig {
+		additionalWidth = if (width < 0F) 0F else width
+		additionalHeight = if (height < 0F) 0F else height
+		return this
+	}
+
+	fun setAdditionalMargin(marginHorizontal: Float = this.additionalMarginHorizontal, marginVertical: Float = this.additionalMarginVertical): CircleImageViewConfig {
+		additionalMarginHorizontal = marginHorizontal
+		additionalMarginVertical = marginVertical
+		return this
+	}
+
+	fun copy(config: CircleImageViewConfig) {
+		isDrawCircle(config.drawCircle)
+		isDrawBorder(config.drawBorder)
+		isDrawAdditional(config.drawAdditional)
+		setBorderWidth(config.borderWidth)
+		setBorderColor(config.borderColor)
+		setCircleRadius(config.circleRadius)
+		setCenter(config.centerX, config.centerY)
+		setAdditionalLocation(config.additionalLocation)
+		setAdditionalSize(config.additionalWidth, config.additionalHeight)
+		setAdditionalMargin(config.additionalMarginHorizontal, config.additionalMarginVertical)
+	}
 }
